a459db30b4b84a2fac5eab12d2fc335421b6747a
ÉLio Laender <laenderquadros@gmail.com>
2018-07-24 15:49:00 -0300

    Update DownloadsDAO.php

--- dama/dao/AccessObject/DownloadsDAO.php
+++ dama/dao/AccessObject/DownloadsDAO.php
@@ -2,36 +2,31 @@
 
 include_once "model/DownloadsModel.php";
 
-class DownloadsDAO extends DownloadsModel{
-
+class DownloadsDAO extends DownloadsModel
+{
     private $insert = "",
             $update = "UPDATE Downloads SET downloads_cont = (downloads_cont+1);",
             $select = "SELECT downloads_cont FROM Downloads;";
-
-
+    
     #atualiza quantidade de downloads realizados.
-    public function downIncrement(){
-
-        if($this->runQuery($this->update)){
-
-        } else {
-
+    public function downIncrement()
+    {
+        if($this->runQuery($this->update))
+        {
+        } else 
+        {
             echo "Não foi possível realizar o update";
-
         }
-
     }
-
+    
     #retorna quantidade de dowwnloads
-    public function qtDowns(){
-
-         if($row = $this->runSelect($this->select)){
-
+    public function qtDowns()
+    {
+         if($row = $this->runSelect($this->select))
+         {
              return $row[0]['downloads_cont'];
-
-         } else {
-
+         } else 
+         {
          }
-
     }
 }
