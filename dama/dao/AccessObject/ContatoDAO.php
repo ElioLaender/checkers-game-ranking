2288a98c8a7b5aced61502703d6d8b9dd070c914
ÉLio Laender <laenderquadros@gmail.com>
2018-07-24 15:46:48 -0300

    Update ContatoDAO.php

--- dama/dao/AccessObject/ContatoDAO.php
+++ dama/dao/AccessObject/ContatoDAO.php
@@ -1,40 +1,33 @@
 <?php
 
-
 require_once "model/ContatoModel.php";
 
-class ContatoDAO extends ContatoModel {
-
+class ContatoDAO extends ContatoModel 
+{
     private $insert = "INSERT INTO Contato (contato_nome,contato_email,contato_assunto,contato_mensagem) VALUES ('%s','%s','%s','%s');";
 
-        public function persistContact($nome,$email,$assunto,$mensagem) {
-
+        public function persistContact($nome,$email,$assunto,$mensagem) 
+        {
             $this->setContatoNome($nome);
             $this->setContatoEmail($email);
             $this->setContatoAssunto($assunto);
             $this->setContatoMensagem($mensagem);
 
-            $this->insert = sprintf($this->insert, $this->getContatoNome(),
-                                                   $this->getContatoEmail(),
-                                                   $this->getContatoAssunto(),
-                                                   $this->getContatoMensagem()
-                                                    );
-
-            try {
-
-
+            $this->insert = sprintf
+                (
+                    $this->insert, 
+                    $this->getContatoNome(),
+                    $this->getContatoEmail(),
+                    $this->getContatoAssunto(),
+                    $this->getContatoMensagem()
+                );
+            try 
+            {
                 $this->runQuery($this->insert);
-
-            }  catch (Exception $e) {
-
+            }  catch (Exception $e) 
+            {
                 var_dump($e->getMessage());
-
             }
-
-
-
-
-
         }
 
 }
