16fc38422d72c6379e1efd2c3b6dcd09492771c6
ÉLio Laender <laenderquadros@gmail.com>
2018-07-24 15:57:23 -0300

    Update ViewConfig.php

--- dama/config/ViewConfig.php
+++ dama/config/ViewConfig.php
@@ -5,23 +5,17 @@
  * Date: 11/09/15
  * Time: 12:11
  */
-
 /**
  * A classe visão é responsável por armazenar dados para apresentação num
  * determinado arquivo de visão php
  */
-
 include_once 'config/RouteConfig.php';
 
 class ViewConfig {
     /**
      * Lista de dados para serem recuperados e impressos dentro de uma view
      */
-
     public $dados = array();
-
-
-
     /**
      * Adiciona valores no vetor, que na view será transformado em variáveis.
      *
@@ -30,7 +24,8 @@ class ViewConfig {
      * @return void
      */
 
-    public function set($nome, $valor){
+    public function set($nome, $valor)
+    {
         $this->dados[$nome] = $valor;
     }
 
@@ -44,11 +39,10 @@ class ViewConfig {
      * @return void
      */
 
-    public function bind($nome, $valor){
-
+    public function bind($nome, $valor)
+    {
         #armazena valor da variável como referido.
         $this->dados[$nome] = &$valor;
-
     }
 
     /**
@@ -60,27 +54,25 @@ class ViewConfig {
 
     public function get($nome='')
     {
-
         /*
          * Se não existir nenhuma variável com o nome indicado como parâmetro,
          * o método retorna uma string vazia.
          */
-
-        if ($nome == '') {
+        if ($nome == '') 
+        {
             return $this->dados;
-
-        } else {
-            if (isset($this->dados[$nome]) && ($this->dados[$nome] != '')) {
+        } else 
+        {
+            if (isset($this->dados[$nome]) && ($this->dados[$nome] != '')) 
+            {
                 return $this->dados[$nome];
-            } else {
+            } else 
+            {
                 return '';
             }
-
         }
-
     } # encerra function get.
-
-
+    
     /**
      * renderiza um arquivo de visão específico com as variáveis armazenadas internamente.
      * Como resultado, envia conteúdo html para o navegador do usuário.
@@ -89,34 +81,28 @@ class ViewConfig {
      * @return void
      */
 
-    public function render($arquivo){
-
-
-
+    public function render($arquivo)
+    {
         $route = RouteConfig::rotas();
-
-
+        
         /*
          * Transforma cada item armazenado na lista de dados em variáveis locais.
          */
 
-        foreach($this->get() as $chave => $item){ //Dá um foreach no get() da ViewController.
+        foreach($this->get() as $chave => $item)
+        { //Dá um foreach no get() da ViewController.
             $$chave = $item; //$$ referencia ao valor do nome, não o valor em si.
         }
-
 
         /*
          * Procura o arquivo php dentro da pasta visões.
          * Se o arquivo existir, inclui o mesmo dentro da função, rederizando e executando o conteúdo dentro dele.
          */
 
-        if(file_exists($route['VIEW_DIR'] . $arquivo)){
-
+        if(file_exists($route['VIEW_DIR'] . $arquivo))
+        {
             include_once $route['VIEW_DIR'] . $arquivo;
         }
-
     }
-
-
 }
 
